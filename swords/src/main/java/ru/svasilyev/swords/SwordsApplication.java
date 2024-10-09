96e65702c33f57e8edd25ec11b40efe0893c75b4
SVasi1yev <vsemenm@gmail.com>
2024-10-09 19:29:27 +0300

    init commit

---
+++ swords/src/main/java/ru/svasilyev/swords/SwordsApplication.java
@@ -0,0 +1,13 @@
+package ru.svasilyev.swords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwordsApplication.class, args);
+	}
+
+}
